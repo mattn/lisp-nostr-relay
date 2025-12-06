6c8ca14f22242d2098bee14dc7c805e401d8c973
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2025-12-06 23:48:31 +0900

    quiet

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-sbcl --non-interactive --load nostr-relay.lisp --eval '(nostr-relay:main)'
+sbcl --lose-on-corruption --noinform --disable-debugger --non-interactive --noprint --load nostr-relay.lisp --eval '(nostr-relay:main)'
