dfa5c571f902fa3b85a8b1374a6167d654efd151
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2026-01-15 09:55:18 +0900

    update postmodern::connection

--- nostr-relay.lisp
+++ nostr-relay.lisp
@@ -111,7 +111,7 @@
                                :port (getf *db-config* :port)
                                :pooled-p nil
                                :use-ssl :try)))
-            (postmodern:*database* conn)
+            (setf (postmodern:*database*) conn)
             (format t "Database connected successfully~%")
             t))
       (error (e)
